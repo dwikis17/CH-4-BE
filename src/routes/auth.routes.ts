8049124ad37fe2cd9663ccb619cedd6f6c2726d4
dwiki <dwikis17@gmail.com>
2025-08-14 18:16:01 +0800

    add get

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -16,7 +16,7 @@ const router = Router();
  */
 router.post('/register', registerValidation, validate, authController.register);
 
-router.post('/callback', authController.callback);
+router.get('/callback', authController.callback);
 
 /**
  * @route POST /api/auth/login
